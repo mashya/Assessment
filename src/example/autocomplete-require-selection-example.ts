ae0a5d6a4cca61d773089dda96996d7cfdacadc3
Ashya <minnamareddyashya@gmail.com>
2024-04-25 14:22:53 +0200

    code changes

---
+++ src/example/autocomplete-require-selection-example.ts
@@ -0,0 +1,96 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  Observable,
+  debounceTime,
+  delay,
+  distinctUntilChanged,
+  map,
+  of,
+  switchMap,
+} from 'rxjs';
+
+/**
+ * @title Require an autocomplete option to be selected
+ */
+@Component({
+  selector: 'autocomplete-require-selection-example',
+  templateUrl: 'autocomplete-require-selection-example.html',
+  styleUrl: 'autocomplete-require-selection-example.css',
+  standalone: true,
+  imports: [
+    FormsModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+  ],
+})
+export class AutocompleteRequireSelectionExample {
+  options$: Observable<string[]>;
+  myControl = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(10),
+  ]);
+  lastInput = '';
+  lastTime = 0;
+  ngOnInit() {
+    //console.log('entered nginit');
+    this.options$ = this.myControl.valueChanges.pipe(
+      // Trim leading/trailing whitespace and ignore empty strings
+
+      map((value) => value?.trim()),
+      // // Debounce to avoid excessive requests
+      debounceTime(300),
+      // // Only proceed if input has changed or 300ms have passed since last emission
+      distinctUntilChanged(
+        (x, y) => x === y && Date.now() - this.lastTime >= 300
+      ),
+      // // Filter based on minimum input length
+      map((value) => (value != undefined && value?.length >= 3 ? value : '')),
+      // // Clear options if input length is less than 3
+      switchMap((value) => {
+        if (value) {
+          console.log('value eneted', value);
+          this.lastInput = value;
+          this.lastTime = Date.now();
+          return this.slowResponse(value);
+        } else {
+          return of([]);
+        }
+      })
+      // Map service response to options
+      //map(values => values.map(value => ({ value })))
+    );
+  }
+  displayError() {
+    //console.log('entered check');
+    if (this.myControl.hasError('required')) {
+      return 'Value is required';
+    } else if (this.myControl.hasError('maxlength')) {
+      return 'Maximum length is 10 characters';
+    }
+    return '';
+  }
+
+  slowResponse(value: string): Observable<string[]> {
+    return of(value ? [value + '1', value + '2', value + '3'] : []).pipe(
+      delay(3000)
+    );
+  }
+}
+
+/**  Copyright 2024 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
